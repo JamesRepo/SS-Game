f971054844b196b9a5c7572d202368df6b49a9e1
JamesRepo <jelner@rocketmail.com>
2018-07-02 20:02:49 +0100

    2nd

---
+++ SS-Game/Assets/Scripts/CreateGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGrid : MonoBehaviour {
+
+    public Transform gridCellPrefab;
+
+    public Vector3 gridSize;
+
+	// Use this for initialization
+	void Start () {
+        GridCreation();
+	}
+
+    void GridCreation () {
+        for (int x = 0; x < gridSize.x; x++) {
+            for (int z = 0; z < gridSize.z; z++) {
+                Instantiate(gridCellPrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+
+}
