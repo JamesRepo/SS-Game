870906fcc6c70fdaa9c33399997296ce090e5266
JamesRepo <jelner@rocketmail.com>
2018-07-06 04:39:01 +0100

    basic game

---
+++ SS-Game/Assets/Scripts/Cube.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+    private float fall = 0;
+    public float fallSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckuserInput();
+        CheckForNew();
+	}
+
+    void CheckuserInput() {
+
+        if(Input.GetKeyDown(KeyCode.RightArrow)) {
+
+            transform.position += new Vector3(1, 0, 0);
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, 0, 1);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0, -1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) || Time.time - fall >= fallSpeed) 
+        {
+            transform.position += new Vector3(0, -1, 0);
+            fall = Time.time;
+        }
+        
+    }
+
+    void CheckForNew() {
+        if ((int)transform.position.y == 0) {
+            enabled = false;
+            FindObjectOfType<Game>().SpawnNewCube();
+        }
+    }
+
+    //bool CheckIsValid() {
+
+    //    foreach(Transform mino in transform) {
+    //        Vector3 pos = FindObjectOfType<Game>().Round(mino.position);
+
+    //        if(FindObjectOfType<Game>().CheckInGrid(pos) == false) {
+    //            return false;
+    //        }
+    //    }
+    //    return true;
+    //}
+}
---
+++ SS-Game/Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public GameObject Prefab;
+
+    private int height = 10;
+    private int width = 10;
+
+	// Use this for initialization
+	void Start () {
+        SpawnNewCube();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //public bool CheckInGrid(Vector3 position) {
+    //    return ((int)position.x >= 0 && (int)position.x < width && (int)position.y >= 0);
+    //}
+
+    //public Vector3 Round (Vector3 position) {
+    //    return new Vector3(Mathf.Round(position.x), Mathf.Round(position.y), Mathf.Round(position.x));
+    //}
+
+    public void SpawnNewCube() {
+        GameObject nextCube = (GameObject)Instantiate(Prefab, new Vector3(5, 10, 5), Quaternion.identity);
+    }
+}
